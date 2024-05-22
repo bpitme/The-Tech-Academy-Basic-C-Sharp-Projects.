f0605fd2e31b9347a0796cd295151b646f9ed86d
bpitme <128807311+bpitme@users.noreply.github.com>
2024-05-22 22:49:58 +0100

    i m uploading new assignment
    this is new method class assignment

---
+++ MathMethod2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method__Class_Assignment_2
+{
+    internal class MathMethod2
+    {
+        //First method that takes an integer
+        public int MathOp (int number) 
+        {
+            return number +10;
+        }
+        //Second method that takes a decimal
+        public int MathOp (decimal number)
+        {
+            //Convert the decimal to an int to perform math opration
+            int newnum=Convert.ToInt32(number);
+            return newnum *50;
+        }
+        //Third MathOp method that takes a string
+        public int MathOp(string number)
+        {
+            //Convert the string to an int perform math opration 
+            int newnum = Convert.ToInt32(number);
+            return newnum %7;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,18 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Method_Assignment
+namespace Method__Class_Assignment_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book("Harry Potter","JK Rowling",400);
-            Book book2 = new Book("Lord of the Rings", "Tolken",700);
-            book2.pages = 850;
-            
-            Console.WriteLine(book2.pages);
-
+            //Instantiate the MathMethod2 class
+            MathMethod2 math1 = new MathMethod2();
+            // call the first method with  an int parameter
+            Console.WriteLine(math1.MathOp(10));
+            // call the second method with a decimal parameter
+            Console.WriteLine(math1.MathOp(10.3m));
+            // call the third method with a string parameter
+            Console.WriteLine(math1.MathOp("10"));
             Console.ReadLine();
         }
     }
