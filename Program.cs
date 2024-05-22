7488be4c0a639055c55a59ffaa26b100f409e8d1
bpitme <128807311+bpitme@users.noreply.github.com>
2024-05-22 14:39:16 +0100

    Method Class Assignment
    Hi This is Method Class Assignment of C#.Framework Part 2

--- Program.cs
+++ Program.cs
@@ -1,31 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Remoting.Lifetime;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Branching_Assignment
+namespace Method_Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Package Express.");
+            Book book1 = new Book("Harry Potter","JK Rowling",400);
+            Book book2 = new Book("Lord of the Rings", "Tolken",700);
+            book2.pages = 850;
+            
+            Console.WriteLine(book2.pages);
+
             Console.ReadLine();
-           int package weight = 40;
-           int package width = 10;
-           int package height= 12;
-           int package length = 11;
-           if (package weight + package width >10)
-                Console.WriteLine(" greter then the 10");
-            else
-            {
-                Console.WriteLine("Package widhth not greter then 10");
-            }
-
-            int Your estimated total for shipping this package is: $528.00 Thank you!;
         }
-
     }
 }
