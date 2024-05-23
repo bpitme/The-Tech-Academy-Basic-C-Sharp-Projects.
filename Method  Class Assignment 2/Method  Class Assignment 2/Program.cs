fb28b0b03756f131ea059ffcdc78a3cfa0373d9d
bpitme <128807311+bpitme@users.noreply.github.com>
2024-05-23 01:52:42 +0100

    new assignment

---
+++ Method  Class Assignment 2/Method  Class Assignment 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method__Class_Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the MathMethod2 class
+            MathMethod2
+        }
+    }
+}
---
+++ Method  Class Assignment 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method__Class_Assignment_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the MathMethod2 class
+            MathMethod2 math1 = new MathMethod2();
+            // call the first method with  an int parameter
+            Console.WriteLine(math1.MathOp(10));
+            // call the second method with a decimal parameter
+            Console.WriteLine(math1.MathOp(10.3m));
+            // call the third method with a string parameter
+            Console.WriteLine(math1.MathOp("10"));
+            Console.ReadLine();
+        }
+    }
+}
